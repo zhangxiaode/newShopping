5902238565cbb34b6ab66ed247b5948da0a74bad
zhangxiaode <2279323443@qq.com>
2018-10-16 16:16:51 +0800

    重构

--- pages/userCenter/index.js
+++ pages/userCenter/index.js
@@ -3,32 +3,9 @@ const app = getApp()
 
 Page({
   data: {
-    orderData:{
-      showArrow: true,
-      arrowL: '我的订单',
-      arrowR: '查看全部订单',
-      icon: ''
-    },
-    aboutData:{
-      showArrow: true,
-      arrowL: '关于我们',
-      arrowR: '',
-      icon: '../../assets/info.svg'
-    }
-  },
-  goAbout(){
-    wx.navigateTo({
-      url: '../about/index'
-    })
-  },
-  goOrder(event){
-    wx.navigateTo({
-      url: '../order/index?index=' + event.currentTarget.dataset.index
-    })
   },
   onLoad(){
   },
   onShow() {
-    
   }
 })
